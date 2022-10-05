919033396cfc1aeda33c6cdc9e4792e260793060
tommyli97 <tommyli76278@gmail.com>
2022-10-05 14:34:28 -0700

    Completed tiny cows generator, updated css to be responsive

--- client/components/Game.js
+++ client/components/Game.js
@@ -1,18 +1,20 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import CowContainer from './Cow';
 import ShopContainer from './Shop';
 import './styles/Game.css'
 import { connect, useStore, useDispatch } from 'react-redux';
-import {calculateActionCreator} from '../actions/actions.js';
+import { calculateActionCreator } from '../actions/actions.js';
 
 //game body component
 export function Game(props) {
+    // console.log('stats', props.stats)
+
     const [timer, setTimer] = useState(0)
     const dispatch = useDispatch()
     //setInterval ( call to dispatch (CALCULATE), 5000 ms)
     useEffect(() => {
         //runs every 5 seconds
-        console.log("DISPATCHER!: ", timer)
+        // console.log("DISPATCHER!: ", timer)
         dispatch(calculateActionCreator())
 
         //triggers the new timer
@@ -23,7 +25,7 @@ export function Game(props) {
 
     return (
         <div className="gameBody">
-            <CowContainer />
+            <CowContainer stats={props.stats} />
             <ShopContainer />
         </div>
     )
