5dec2d3877923cfb1323fd420702096d56992213
Kat <kattsaib@gmail.com>
2022-11-29 21:48:06 -0800

    kat demo version

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt');
 
-const MONGO_URI = "mongodb+srv://cera:Dgafes1234!@cluster0.fvxj16i.mongodb.net/?retryWrites=true&w=majority";
+const MONGO_URI = "mongodb+srv://katalystkat:<password>@fitdevs.s392t6z.mongodb.net/?retryWrites=true&w=majority";
 
 //change this
 mongoose.connect(MONGO_URI, {
